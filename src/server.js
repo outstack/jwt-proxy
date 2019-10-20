f89cde045287a1256331debd562020badc3dbbb1
Adam Quaile <adamquaile@gmail.com>
2019-10-20 15:37:54 +0100

    Added json schema custom validation

--- src/server.js
+++ src/server.js
@@ -1,14 +1,28 @@
 var http = require('http'),
     httpProxy = require('http-proxy'),
     process = require('process'),
-    jwt = require('jsonwebtoken');
+    jwt = require('jsonwebtoken'),
+    Ajv = require('ajv');
+var ajv = new Ajv();
 
 var JWT_PUBLIC_KEY = process.env.JWT_PUBLIC_KEY.replace(/\\n/g, "\n");
+var JWT_ALGORITHM = process.env.JWT_ALGORITHM || 'RS256';
 var JWT_REQUIREMENTS = {
-    algorithms: 'RS256',
-    audience: process.env.JWT_AUDIENCE,
-    issuer: process.env.JWT_ISSUER
+    algorithms: JWT_ALGORITHM
 };
+
+if (process.env.JWT_AUDIENCE) {
+    JWT_REQUIREMENTS.audience = process.env.JWT_AUDIENCE;
+}
+if (process.env.JWT_ISSUER) {
+    JWT_REQUIREMENTS.issuer = process.env.JWT_ISSUER;
+}
+
+var JWT_VALIDATE_SCHEMA = null;
+if (process.env.JWT_VALIDATE_JSON_SCHEMA) {
+    JWT_VALIDATE_SCHEMA = JSON.parse(process.env.JWT_VALIDATE_JSON_SCHEMA);
+}
+
 var ALLOW_ANONYMOUS = process.env.ALLOW_ANONYMOUS === "1";
 var LISTEN_PORT = process.env.LISTEN_PORT;
 
@@ -39,16 +53,33 @@ var server = http.createServer(function(req, res) {
     }
 
     if (!decoded && !ALLOW_ANONYMOUS) {
+        console.log("Token not verified against requirements or public key");
         res.writeHead(401, { 'Content-Type': 'text/plain' });
         res.end('Unauthorized');
         return;
     }
 
-  proxy.web(req, res, {
+    if (JWT_VALIDATE_SCHEMA) {
+        if (!ajv.compile(JWT_VALIDATE_SCHEMA)(decoded)) {
+            console.log("Token failed json schema validation");
+            res.writeHead(401, { 'Content-Type': 'text/plain' });
+            res.end('Unauthorized');
+            return;
+        }
+    }
+
+
+    proxy.web(req, res, {
     target: process.env.UPSTREAM_URL
   });
 });
 
 
 console.log("listening on port " + LISTEN_PORT)
+console.debug("Debug info:", {
+    jwt_requirements: JWT_REQUIREMENTS,
+    jwt_algorithm: JWT_ALGORITHM,
+    jwt_public_key: JWT_PUBLIC_KEY,
+    jwt_validate_schema: JWT_VALIDATE_SCHEMA
+});
 server.listen(LISTEN_PORT);
